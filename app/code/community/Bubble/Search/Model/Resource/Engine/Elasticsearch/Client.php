ce402db8282acd374b8a635583526a1863baf98e
hpanke <henning.panke@googlemail.com>
2013-08-23 12:27:00 +0200

    fix index preparation
    The index preparation tried to update the settings of a existing
    index. But that led to to an error because the index as to be closed
    before updating "non dynamic" settings like analyzers and filters.
    So for now reconfiguration of existing index is disabled.

--- app/code/community/Bubble/Search/Model/Resource/Engine/Elasticsearch/Client.php
+++ app/code/community/Bubble/Search/Model/Resource/Engine/Elasticsearch/Client.php
@@ -750,8 +750,10 @@ class Bubble_Search_Model_Resource_Engine_Elasticsearch_Client extends Elastica\
             if (!$index->exists()) {
                 $indexSettings['number_of_shards'] = (int) $this->getConfig('number_of_shards');
                 $index->create($indexSettings);
-            } else {
+            } elseif ($this->_shouldReconfigureIndex()) {
+                $index->close();
                 $index->setSettings($indexSettings);
+                $index->open();
             }
             $mapping = new Elastica\Type\Mapping();
             $mapping->setType($index->getType('product'));
@@ -763,5 +765,10 @@ class Bubble_Search_Model_Resource_Engine_Elasticsearch_Client extends Elastica\
         }
 
         return $this;
+    }
+    
+    protected function _shouldReconfigureIndex()
+    {
+        return false;
     }
 }
